a089edd801c2d4f9ffaa4646d74447c0b0dc6bea
가울림 (Gaulim) <mrjozzz@gmail.com>
2018-04-19 14:59:46 +0900

    iOS 샘플 프로젝트 생성
    우선 4개 프로젝트를 워크스페이스 하나로 묶어서 생성했고,
    추후에 워크스페이스에 다른 프로젝트가 더 추가될 수 있다.
    
    프로젝트 중 'index'는 샘플에 대한 정보를 목록 형식으로 보여주고,
    목록을 누르면 해당 샘플이 있는 앱을 띄워서 실행시킨다.
    나머지 3개(common, view, ar)는 각 샘플을 구현한 앱이다.
    
     - [추가] Xcode 워크스페이스
    
     - [추가] 'index' 프로젝트 (Master-Detail App)
    
     - [추가] 'common' 프로젝트 (Single View App)
    
     - [추가] 'view' 프로젝트 (Single View App)
    
     - [추가] 'ar' 프로젝트 (Augmented Reality App)

---
+++ view/view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  view
+//
+//  Created by 조성현 on 2018. 4. 17..
+//  Copyright © 2018년 gaulim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
